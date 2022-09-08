6c06150d7a406a3fbb63f4cf3f33efd2ebfbe550
krmckmz <keremcokmez@gmail.com>
2022-09-09 01:23:09 +0300

    first commit

---
+++ CAPalindromicSubstring/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "twbiqwtafgjbtolwprpdnymaatlbuacrmzzwhkpvuwdtyfjsbsqxrlxxtqkjlpkvpxmlajdmnyepsmczmmfdtjfbyybotpoebilayqzvqztqgddpcgpelwmriwmoeeilpetbxoyktizwcqeeivzgxacuotnlzutdowiudwuqnghjgoeyojikjhlmcsrctvnahnoapmkcrqmwixpbtirkasbyajenknuccojooxfwdeflmxoueasvuovcayisflogtpxtbvcxfmydjupwihnxlpuxxcclbhvutvvshcaikuedhyuksbwwjsnssizoedjkbybwndxpkwcdxaexagazztuiuxphxcedqstahmevkwlayktrubjypzpaiwexkwbxcrqhkpqevhxbyipkmlqmmmogrnexsztsbkvebjgybrolttvnidnntpgvsawgaobycfaaviljsvyuaanguhohsepbthgjyqkicyaxkytshqmtxhilcjxdpcbmvnpippdrpggyohwyswuydyrhczlxyyzregpvxyfwpzvmjuukswcgpenygmnfwdlryobeginxwqjhxtmbpnccwdaylhvtkgjpeyydkxcqarkwvrmwbxeetmhyoudfuuwxcviabkqyhrvxbjmqcqgjjepmalyppymatylhdrazxytixtwwqqqlrcusxyxzymrnryyernrxbgrphsioxrxhmxwzsytmhnosnrpwtphaunprdtbpwapgjjqcnykgspjsxslxztfsuflijbeebwyyowjzpsbjcdabxmxhtweppffglvhfloprfavduzbgkw";
+        var result = LongestPalindrome(s);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+    public static string LongestPalindrome(string s)
+    {
+        string longestPalindrome = string.Empty;
+
+        if (IsPalindrome(s))
+            return s;
+        else
+            for (int i = 0; i < s.Length; i++)
+                for (int j = i; j <= s.Length; j++)
+                {
+                    string palindromeCandidant = s.Substring(i, j - i);
+                    bool isPalindrome = IsPalindrome(palindromeCandidant);
+
+                    if (isPalindrome && palindromeCandidant.Length > longestPalindrome.Length)
+                        longestPalindrome = palindromeCandidant;
+                }
+
+        return longestPalindrome;
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        char[] palindromeCandidantChars = s.ToCharArray();
+        Array.Reverse(palindromeCandidantChars);
+        bool isPalindrome = new String(palindromeCandidantChars) == s ? true : default(bool);
+
+        return isPalindrome;
+    }
+}
